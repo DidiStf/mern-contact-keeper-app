5b87e251b8ea650b0ffcaaa8b9136549e6533efa
DidiStf <dilyana.a.tefanova@gmail.com>
2020-09-19 18:58:49 +0200

    feat: api finished and tested

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,118 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
+
+const authenticate = require('../middleware/auth');
+const Contact = require('../models/Contact');
+const User = require('../models/User');
+
 const router = express.Router();
 
 // @route GET api/contacts
 // @desc Get all user's contacts
 // @access Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', authenticate, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error.' });
+  }
 });
 
 // @route POST api/contacts
 // @desc Add new contact
 // @access Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [authenticate, [body('name', 'Name is required.').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Server Error.' });
+    }
+  }
+);
 
 // @route PUT api/contacts/:id
 // @desc Update contact
 // @access Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
+router.put('/:id', authenticate, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+  const { id } = req.params;
+
+  //Build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(id);
+
+    if (!contact) return res.status(404).json({ msg: 'Contact not found.' });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: 'Non authorized.' });
+
+    contact = await Contact.findByIdAndUpdate(
+      id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error.' });
+  }
 });
 
 // @route DELETE api/contacts/:id
 // @desc Delete contact
 // @access Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+router.delete('/:id', authenticate, async (req, res) => {
+  const { id } = req.params;
+  try {
+    let contact = await Contact.findById(id);
+
+    if (!contact) return res.status(404).json({ msg: 'Contact not found.' });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: 'Non authorized.' });
+
+    await Contact.findByIdAndRemove(id);
+
+    res.json({ msg: 'Contact removed.' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error.' });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 
+const connectDB = require('./config/db');
+
 const auth = require('./routes/auth');
 const contacts = require('./routes/contacts');
 const users = require('./routes/users');
@@ -10,6 +12,14 @@ const users = require('./routes/users');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//  Connect to Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper' }));
 
 // Define Routes
 app.use('/api/auth', auth);
