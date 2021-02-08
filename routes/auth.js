af8042ebb117d4503cba2a154286da6c6d8ef48b
DidiStf <dilyana.a.stefanova@gmail.com>
2021-02-08 13:50:12 +0100

    fix (server): error message for incalid credentials on invalid email on login

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,7 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ message: 'Invalid credentials' });
     }
 
     const { email, password } = req.body;
